4ec074799c8b6cc45b177080143af308dabce6dc
Moonshone <mahtab.kadkhoda@gmx.de>
2026-09-11 01:03:16 +0200

    Update database.local.php

--- config/database.local.php
+++ config/database.local.php
@@ -1,1 +1,10 @@
+<?php
 
+// Copy to database.local.php on the server and enter the real password there.
+// Never commit database.local.php.
+return [
+    'host' => 'mysql.lalehbarzegar.com',
+    'dbname' => 'neweshtaniha',
+    'user' => 'laleh',
+    'password' => 'Farrokhmehr@1327',
+];
